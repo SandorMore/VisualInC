fae44a8879b5185890fab7dc0eac0a843ab63bc8
SandorMore <msaso0725@gmail.com>
2025-02-14 13:17:54 +0100

    fun

--- pointersGyak2.c
+++ pointersGyak2.c
@@ -4,5 +4,10 @@ int main(){
     int a = 1025;
     int *pA = &a;
     printf("The size of datatype int is: %d", sizeof(int));
+    printf("\nAddress of a is: %p, and value of a is %d", pA, *pA);
+    char *p0;
+    p0 = (char*)pA;
+    printf("\nThe size of datatype char is: %d", sizeof(char));
+    printf("\nAddress of a is: %p, and value of a is %d", p0, *p0);
     return 0;
 }
